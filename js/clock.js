8e6ccc8e793b5c9612e9cdc029e7c7cdca18c82c
Mike McAllister <mike@logikbox.com>
2022-08-28 13:37:34 -0400

    revision 2, jquery removed from js

--- js/clock.js
+++ js/clock.js
@@ -19,11 +19,13 @@ let clockhour = [
 	'hTweleve',
 ];
 
-let toPast = '';
-let greater30 = 0;
-let faceMinutes = 'oclock';
+let toPast = 'oclock';
+let greater30 = 1;
+let faceMinutes = '';
 
-const timerFace = document.getElementById('digitialclock');
+const digitialclock = document.getElementById('digitialclock');
+const clockFaceItems = document.querySelectorAll('.clockface');
+const seconds = document.querySelectorAll('.glyphicon');
 
 function clockTimer() {
 	time = new Date();
@@ -33,86 +35,103 @@ function clockTimer() {
 	currentSec = time.getSeconds();
 	currentHour >= 12 ? (ampm = 'PM') : (ampm = 'AM');
 
-	writeNewTime();
+	setTime();
 }
 
-const writeNewTime = () => {
+const setTime = () => {
 	currentHour = currentHour % 12;
 	if (currentHour == 0) currentHour = 12;
-	if (currentMin < 10) currentMin = '0' + currentMin;
+	if (currentMin < 10) currentMin = `0${currentMin}`;
 	if (currentSec < 10) currentSec = `0${currentSec}`;
 
-	let printTime = `${currentHour}:${currentMin}:${currentSec} ${ampm}`;
+	const printTime = `${currentHour}:${currentMin}:${currentSec} ${ampm}`;
 
-	timerFace.innerText = printTime;
+	digitialclock.innerText = printTime;
 
 	updateClock();
 	updateSec(currentSec);
 };
 
-function updateClock() {
-	greater30 = 0;
+const updateMinutes = () => {
+	if (currentMin >= 0 && currentMin < 5) {
+		faceMinutes = '';
+	} else if (
+		(currentMin >= 5 && currentMin < 10) ||
+		(currentMin >= 55 && currentMin < 60)
+	) {
+		faceMinutes = 'five';
+	} else if (
+		(currentMin >= 10 && currentMin < 15) ||
+		(currentMin >= 50 && currentMin < 55)
+	) {
+		faceMinutes = 'ten';
+	} else if (
+		(currentMin >= 15 && currentMin < 20) ||
+		(currentMin >= 45 && currentMin < 50)
+	) {
+		faceMinutes = 'quarter';
+	} else if (
+		(currentMin >= 20 && currentMin < 25) ||
+		(currentMin >= 40 && currentMin < 45)
+	) {
+		faceMinutes = 'twenty';
+	} else if (
+		(currentMin >= 25 && currentMin < 30) ||
+		(currentMin >= 35 && currentMin < 40)
+	) {
+		faceMinutes = 'special25';
+	} else {
+		faceMinutes = 'half';
+	}
 
-	currentHour % 12 === 0 ? (currentHour = 12) : (currentHour = currentHour);
+	updateScreen();
+};
 
-	console.log(`${currentHour}:${currentMin}:${currentSec}`);
+const updateClock = () => {
+	greater30 = 1;
 
-	if (currentMin === 0) {
+	if (currentMin >= 0 && currentMin < 5) {
 		toPast = 'oclock';
-	} else if (currentMin >= 1 && currentMin <= 30) {
+	} else if (currentMin >= 5 && currentMin <= 30) {
 		toPast = 'past';
 	} else {
 		toPast = 'to';
 		greater30 = 0;
 	}
-
-	if (currentMin === 0 && currentMin < 5) {
-		faceMinutes = '';
-	} else if (currentMin >= 5 && currentMin < 10) {
-		faceMinutes = 'five';
-	} else if (currentMin >= 10 && currentMin < 15) {
-		faceMinutes = 'ten';
-	} else if (currentMin >= 15 && currentMin < 20) {
-		faceMinutes = 'quarter';
-	} else if (currentMin >= 20 && currentMin < 25) {
-		faceMinutes = 'twenty';
-	} else if (currentMin >= 25 && currentMin < 30) {
-		faceMinutes = 'special25';
-	} else if (currentMin >= 30 && currentMin < 35) {
-		faceMinutes = 'half';
-	} else if (currentMin >= 35 && currentMin < 40) {
-		faceMinutes = 'special25';
-	} else if (currentMin >= 40 && currentMin < 45) {
-		faceMinutes = 'twenty';
-	} else if (currentMin >= 45 && currentMin < 50) {
-		faceMinutes = 'quarter';
-	} else if (currentMin >= 50 && currentMin < 55) {
-		faceMinutes = 'ten';
-	} else if (currentMin >= 55 && currentMin < 60) {
-		faceMinutes = 'five';
-	}
-
-	updateScreen();
-}
+	console.log(`${currentHour}:${currentMin}:${currentSec}`);
+	updateMinutes();
+};
 
 const updateSec = (currentSec) => {
-	let sec = currentSec;
-	$('.glyphicon').css('color', '#424447');
-	//console.log(sec);
-	for (i = 0; i <= sec; i++) {
-		$('.glyphicon:nth-of-type(' + (i + 1) + ')').css('color', '#ffffff');
+	seconds.forEach((item) => {
+		item.classList.remove('on');
+	});
+	for (i = 0; i <= currentSec; i++) {
+		document
+			.querySelector(`.glyphicon:nth-of-type(${i + 1})`)
+			.classList.add('on');
 	}
 };
 
 const updateScreen = () => {
-	$('.clockface').css('color', '#424447');
-	$('li#' + toPast).css('color', '#ffffff');
-	$('li#' + ampm).css('color', '#ffffff');
-	$('li#' + clockhour[currentHour - greater30]).css('color', '#FFFFFF');
+	cleanScreen();
+	document.getElementById(toPast).classList.add('on');
+	document.getElementById(ampm).classList.add('on');
+
+	let thisHourId = parseInt(currentHour) - parseInt(greater30);
+	if (thisHourId === 12) thisObj = 0;
+	document.getElementById(clockhour[thisHourId]).classList.add('on');
+
 	if (faceMinutes != 'special25') {
-		$('li#' + faceMinutes).css('color', '#ffffff');
+		document.getElementById(faceMinutes).classList.add('on');
 	} else {
-		$('li#twenty').css('color', '#ffffff');
-		$('li#five').css('color', '#ffffff');
+		document.getElementById('twenty').classList.add('on');
+		document.getElementById('five').classList.add('on');
 	}
+};
+
+const cleanScreen = () => {
+	clockFaceItems.forEach((item) => {
+		item.classList.remove('on');
+	});
 };
